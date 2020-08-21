39ce3e0fe529ba8a4c9fabf89205c199c062277c
Sebastian <66569741+HbHbNr@users.noreply.github.com>
2020-08-21 19:39:45 +0200

    Switch ascii85 to return bytearry; solve not yet adapted

--- ascii85.py
+++ ascii85.py
@@ -11,18 +11,18 @@ def decode(a):
     print('end marker found')
     a = a[2:len(a)-2]
 
-    decoded = ''
+    decoded = bytearray()
     fulltuples = len(a) // 5
     remainder = len(a) % 5
     print(fulltuples)
     print(remainder)
     currentoffset = 0
     for i in range(fulltuples):
-        decoded = decoded + decodeTuple(a[currentoffset:currentoffset+5])
+        decoded += decodeTuple(a[currentoffset:currentoffset+5])
         currentoffset += 5
     if remainder > 0:
         padded = a[currentoffset:len(a)].ljust(5, 'u')
-        decoded = decoded + decodeTuple(padded)[0:4-(5-remainder)]
+        decoded += decodeTuple(padded)[0:4-(5-remainder)]
     return decoded
 
 def decodeTuple(tuple5):
@@ -30,13 +30,13 @@ def decodeTuple(tuple5):
         print('wrong size of tuple: "' + tuple5 + '"')
         return False
     
-    decoded = ''
+    decoded = bytearray()
     charsum = 0
     for i in range(5):
         charsum *= 85
         charsum += (ord(tuple5[i]) - 33)
     for i in range(4):
-        decoded += chr((charsum >> (8 * (3 - i))) % 256)
+        decoded.append((charsum >> (8 * (3 - i))) % 256)
     #print(tuple5)
     #print(decoded)
     return decoded
