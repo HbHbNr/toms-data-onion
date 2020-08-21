f661c4ccd1821d6aa9b5397233de56c428830e5f
Sebastian <66569741+HbHbNr@users.noreply.github.com>
2020-08-21 19:09:20 +0200

    Add more tests

--- ascii85_test.py
+++ ascii85_test.py
@@ -1,4 +1,4 @@
-import ascii85
+from ascii85 import decode, decodeTuple
 
 # GNU General Public License Version 3
 #
@@ -7,5 +7,15 @@ import ascii85
 leviathan_encoded = '''<~9jqo^BlbD-BleB1DJ+*+F(f,q/0JhKF<GL>Cj@.4Gp$d7F!,L7@<6@)/0JDEF<G%<+EV:2F!,O<DJ+*.@<*K0@<6L(Df-\\0Ec5e;DffZ(EZee.Bl.9pF"AGXBPCsi+DGm>@3BB/F*&OCAfu2/AKYi(DIb:@FD,*)+C]U=@3BN#EcYf8ATD3s@q?d$AftVqCh[NqF<G:8+EV:.+Cf>-FD5W8ARlolDIal(DId<j@<?3r@:F%a+D58'ATD4$Bl@l3De:,-DJs`8ARoFb/0JMK@qB4^F!,R<AKZ&-DfTqBG%G>uD.RTpAKYo'+CT/5+Cei#DII?(E,9)oF*2M7/c~>'''
 leviathan_decoded = 'Man is distinguished, not only by his reason, but by this singular passion from other animals, which is a lust of the mind, that by a perseverance of delight in the continued and indefatigable generation of knowledge, exceeds the short vehemence of any carnal pleasure.'
 
-def test_decode():
-    assert ascii85.decode(leviathan_encoded) == leviathan_decoded
+tuples = [('t', 'F8'), ('td', 'FCY'), ('tdo', 'FC]:'), ('tdo!', 'FC];5')]
+
+def test_decode1():
+    assert decode(leviathan_encoded) == leviathan_decoded
+
+def test_decode2():
+    for (plain, encoded) in tuples:
+        assert plain == decode('<~' + encoded + '~>')
+
+def test_decodeTuple():
+    for (plain, encoded) in tuples:
+        assert plain == decodeTuple(encoded.ljust(5, 'u'))[0:len(plain)]
